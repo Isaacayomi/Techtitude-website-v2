b1fa2b232bda5fa85195fcd8104bdcdae77d91b7
Pr.ime <okunlolaa3@outlook.com>
2023-11-10 17:30:09 +0100

    updated hamburger functionality

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,29 +1,43 @@
 import React from 'react'
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client'
 
 import NAV_ICON from '/images/nav-icon.png'
 import TECHTITUDE_LOGO from '/images/techtitude-logo.png'
+import { redirect } from 'react-router-dom';
 // import { NAV_ICON, TECHTITUDE_LOGO } from '../assets/images/img';
 
-
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const ToggleMenu = () => {
+        setOpen(!open);
+        document.querySelector('.NavItems').classList.toggle('open');
+    };
+
+    const handleLinkClick = (url) => {
+        redirect(url);
+    };
+
     return (
         <div className='ContainerNav'>
             <div className='Logo'>
                 <img src={TECHTITUDE_LOGO} alt='Techtitude Logo' className='NavLogo' />
             </div>
 
-            <nav className='NavItems'>
+            <nav className={`NavItems ${open ? 'open active' : ''}`}>
                 <ul>
-                    <li>TESTIMONIALS</li>
-                    <li>COURSES</li>
-                    <li>INSTRUCTORS</li>
-                    <li>SIGN UP NOW</li>
+                    <li onClick={() => handleLinkClick('/testimonials')}>TESTIMONIALS</li>
+                    <li onClick={() => handleLinkClick('/courses')}>COURSES</li>
+                    <li onClick={() => handleLinkClick('/instructors')}>INSTRUCTORS</li>
+                    <li onClick={() => handleLinkClick('/signup')}>SIGN UP NOW</li>
                 </ul>
             </nav>
-
             <div className='NavIcon'>
-                <img src={NAV_ICON} alt='Nav Icon' />
+
+                <button onClick={ToggleMenu}>
+                    <img src={NAV_ICON} alt='Nav Icon' />
+                </button>
             </div>
         </div>
 
